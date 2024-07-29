a7832a3c8ac3c761b501100cf340574edb2948fd
jonathasluis <jonathas_sousasgs@hotmail.com>
2024-07-29 16:34:51 -0300

    finalização aula 1 do curso Java: trabalhando com lambdas, streams e Spring Framework

--- 01-Java-Web-crie-aplicacoes-Spring-Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ 01-Java-Web-crie-aplicacoes-Spring-Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,9 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +17,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Dotenv dotenv = Dotenv.load(); // Carrega o arquivo .env na raiz do projeto
+		String minhaVariavel = dotenv.get("API_KEY");
 		ConsumoApi consumoApi = new ConsumoApi();
-		String json = consumoApi.obterDados("https://viacep.com.br/ws/37206074/json/");
-		System.out.println(json);
+		String url = "http://www.omdbapi.com/?apikey="+minhaVariavel+"&t=Game+of+Thrones";
+		System.out.println(url);
+		String json = consumoApi.obterDados(url);
+		System.out.println(json );
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dados = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
