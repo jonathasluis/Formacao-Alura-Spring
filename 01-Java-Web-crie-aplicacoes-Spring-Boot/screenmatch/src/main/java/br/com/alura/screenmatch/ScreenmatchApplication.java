5d9f952f8ed773068a9106f75a7841acb117b4a0
jonathasluis <jonathas_sousasgs@hotmail.com>
2024-08-05 17:08:28 -0300

    finalização da aula 2 do curso: Java: trabalhando com lambdas, streams e Spring Framework

--- 01-Java-Web-crie-aplicacoes-Spring-Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ 01-Java-Web-crie-aplicacoes-Spring-Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
-import io.github.cdimascio.dotenv.Dotenv;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,15 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Dotenv dotenv = Dotenv.load(); // Carrega o arquivo .env na raiz do projeto
-		String minhaVariavel = dotenv.get("API_KEY");
-		ConsumoApi consumoApi = new ConsumoApi();
-		String url = "http://www.omdbapi.com/?apikey="+minhaVariavel+"&t=Game+of+Thrones";
-		System.out.println(url);
-		String json = consumoApi.obterDados(url);
-		System.out.println(json );
-		ConverteDados converteDados = new ConverteDados();
-		DadosSerie dados = converteDados.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
---
+++ 01-Java-Web-crie-aplicacoes-Spring-Boot/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,41 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitra = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+    private String ENDERECO = "http://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=" + Dotenv.load().get("API_KEY");
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série: ");
+        var nomeSerie = leitra.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+
+        temporadas.forEach(System.out::println);
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+    }
+}
